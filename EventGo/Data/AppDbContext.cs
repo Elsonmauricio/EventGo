b5f2fcfad897eaf423f7864250d97e7004c75fd5
Abdaisy <abadsiyconceicao@epfundao.edu.pt>
2026-03-19 15:19:04 +0000

    conexao com a base de dados

--- EventGo/Data/AppDbContext.cs
+++ EventGo/Data/AppDbContext.cs
@@ -1,6 +1,41 @@
-﻿namespace EventGo.Data
+﻿using Microsoft.EntityFrameworkCore;
+using EventGo.Models;
+
+namespace EventGo.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Organizador> Organizadores { get; set; }
+        public DbSet<Participante> Participantes { get; set; }
+        public DbSet<Inscricao> Inscricoes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Relação 1:N: Organizador -> Eventos
+            modelBuilder.Entity<Evento>()
+                .HasOne(e => e.Organizador)
+                .WithMany(o => o.Eventos)
+                .HasForeignKey(e => e.OrganizadorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Relação N:N via Inscricao: Evento <-> Participante
+            modelBuilder.Entity<Inscricao>()
+                .HasOne(i => i.Evento)
+                .WithMany(e => e.Inscricoes)
+                .HasForeignKey(i => i.EventoId);
+
+            modelBuilder.Entity<Inscricao>()
+                .HasOne(i => i.Participante)
+                .WithMany(p => p.Inscricoes)
+                .HasForeignKey(i => i.ParticipanteId);
+        }
     }
 }
--- EventGo/Models/Inscricao.cs
+++ EventGo/Models/Inscricao.cs
@@ -1,6 +1,17 @@
-﻿namespace EventGo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventGo.Models
 {
     public class Inscricao
     {
+        public int Id { get; set; }
+
+        public int EventoId { get; set; }
+        public Evento? Evento { get; set; }
+
+        public int ParticipanteId { get; set; }
+        public Participante? Participante { get; set; }
+
+        public DateTime DataInscricao { get; set; } = DateTime.Now;
     }
 }
